500d949fc7515ba2bfa22337c6876ba1468b6aea
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-19 11:32:22 -0600

    working ajax call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
-var artist;
-var city;
+
+//old API key 5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG
+
+//Ticket Master AJAx
+//JENN'S CODE
 
 //on click event for search 
 $("#submitBtn").on("click", function (event) {
@@ -18,94 +21,97 @@ $("#submitBtn").on("click", function (event) {
     sessionStorage.clear();
     sessionStorage.setItem("artist", artist);
     sessionStorage.setItem("city", city);
+
+    var TMqueryURL = "https://app.ticketmaster.com/discovery/v2/events?classificationName=music&keyword=" + artist + "&city=" + city + "&apikey=CivSHmaHRiF5tdJGvaAktTdsXl91vzwm";
+    $.ajax({
+        url: TMqueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        if(!response._embedded) {
+            alert('d\'oh');
+        } else {
+            for (var i = 0; i < response._embedded.events.length; i++) {
+                var artisitResponse = response._embedded.events[i].name;
+                var cityResponse = response._embedded.events[i]._embedded.venues[i].city.name;
+    
+                console.log("city response: " + cityResponse);
+                console.log("artist response: " + artisitResponse);
+            }
+        }
+    });
+
 });
 
 $("#artistDisplay").text(sessionStorage.getItem("artist"));
 $("#cityDisplay").text(sessionStorage.getItem("city"));
 
-
-//Ticket Master 
-    TMqueryURL = "https://app.ticketmaster.com/discovery/v2/events?keyword=" + keyword +"&city=" + city + "&apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword"
-    var keyword = $("#artistInput").val().trim();
-    var city = $("#cityInput").val().trim();
-//JENNS CODE
-// function keyword() {
-//     $.ajax ({
-//         url: TMqueryURL,
-//         method: "GET"
-//     }).then(function(response){
-//         console.log(response);
-//         console.log("AJAX keyword:" + keyword);
-//         console.log("AJAX city: " + city);
-//     })
-// } 
-// keyword();
 //_______________________________________________________________________________
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        var x = document.getElementById("location");
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition, showError);
+//     } else {
+//         var x = document.getElementById("location");
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
 
-function showPosition(position) {
-    // var x = document.getElementById("location");
-    // x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+// function showPosition(position) {
+//     // var x = document.getElementById("location");
+//     // x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 
-    // var latlon = position.coords.latitude + "," + position.coords.longitude;
+//     // var latlon = position.coords.latitude + "," + position.coords.longitude;
 
-    $.ajax({
-        type: "GET",
-        url: TMqueryURL,
-        // async: true,
-        // dataType: "json",
-        success: function (response) {
-            console.log(response);
-            var e = document.getElementById("events");
-            e.innerHTML = response.page.totalElements + " events found.";
-            showEvents(response);
-            // initMap(position, response);
-        },
-        error: function (xhr, status, error) {
-            console.log("error: " + error);
-        }
-    });
+//     $.ajax({
+//         type: "GET",
+//         url: TMqueryURL,
+//         // async: true,
+//         // dataType: "json",
+//         success: function (response) {
+//             console.log(response);
+//             var e = document.getElementById("events");
+//             e.innerHTML = response.page.totalElements + " events found.";
+//             showEvents(response);
+//             // initMap(position, response);
+//         },
+//         error: function (xhr, status, error) {
+//             console.log("error: " + error);
+//         }
+//     });
 
-}
+// }
 
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
-}
+// function showError(error) {
+//     switch (error.code) {
+//         case error.PERMISSION_DENIED:
+//             x.innerHTML = "User denied the request for Geolocation."
+//             break;
+//         case error.POSITION_UNAVAILABLE:
+//             x.innerHTML = "Location information is unavailable."
+//             break;
+//         case error.TIMEOUT:
+//             x.innerHTML = "The request to get user location timed out."
+//             break;
+//         case error.UNKNOWN_ERROR:
+//             x.innerHTML = "An unknown error occurred."
+//             break;
+//     }
+// }
 
 
-function showEvents(response) {
-    for (var i = 0; i < response.page.size; i++) {
-        $("#events").append("<p>" + response._embedded.events[i].name + "</p>");
-    }
-}
+// function showEvents(response) {
+//     for (var i = 0; i < response.page.size; i++) {
+//         $("#events").append("<p>" + response._embedded.events[i].name + "</p>");
+//     }
+// }
 
 
 // function initMap(position, response) {
 //     var mapDiv = document.getElementById('map');
 //     var map = new google.maps.Map(mapDiv, {
-//         center: { lat: position.coords.latitude, lng: position.coords.longitude },
-//         zoom: 10
+//         center: center,
+//         zoom: 8
 //     });
 //     for (var i = 0; i < response.page.size; i++) {
 //         addMarker(map, response._embedded.events[i]);
@@ -124,4 +130,4 @@ function showEvents(response) {
 
 
 
-getLocation();
+// getLocation();
