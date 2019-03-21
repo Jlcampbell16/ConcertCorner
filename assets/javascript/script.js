5f275b64a1fa2e87f8909e8888ada0b7a3fb5b2b
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-20 19:16:54 -0600

    moved modal out of onclick event, cleaned up extra code in JS & HTML

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,9 +28,6 @@ $("#submitBtn").on("click", function (event) {
         } else {
             events = [];
             for (var i = 0; i < response._embedded.events.length; i++) {
-                var artistResponse = response._embedded.events[i].name;
-                var cityResponse = response._embedded.events[i]._embedded.venues[0].name;
-
                 var event = {
                     name: response._embedded.events[i].name,
                     location: response._embedded.events[i]._embedded.venues[0].name,
@@ -70,7 +67,6 @@ $("#submitBtn").on("click", function (event) {
             marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
         };
 
-
         function handleLocationError(browserHasGeolocation, infoWindow, pos) {
             infoWindow.setPosition(pos);
             infoWindow.setContent(browserHasGeolocation ?
@@ -78,7 +74,6 @@ $("#submitBtn").on("click", function (event) {
                 'Error: Your browser doesn\'t support geolocation.');
             infoWindow.open(map);
         }
-
     });
 
     $("#artistDisplay").text(sessionStorage.getItem("artist"));
@@ -98,12 +93,14 @@ $("#submitBtn").on("click", function (event) {
             } else {
                 $(cardContent).append("<p>" + events[i].name + "</p>");
             };
+
             // if date not available error else append
             if (!events[i].date) {
                 $(cardContent).append("<p>Unable to find event date</p>");
             } else {
                 $(cardContent).append("<p>" + events[i].date + "</p>");
             };
+
             // if venue is not available error else append
             if (!events[i].location) {
                 $(cardContent).append("<p>Unable to find event venue</p>");
@@ -116,42 +113,21 @@ $("#submitBtn").on("click", function (event) {
                 $(tixButton).append("<p>Unable to find tickets</p>");
             } else {
                 $(tixButton).append("<a href=" + events[i].tixURL + ">Buy Tickets</a>");
-            }
+            };
 
             // if image is not available error else append
             if (!events[i].image) {
                 $(imageContent).append("<p>image not found</p>");
             } else {
                 $(imageContent).append("<img src=" + events[i].image + "></img>");
-            }
+            };
+
             newCard.append(imageContent)
             newCard.append(cardContent);
             cardContent.append(tixButton);
             $(".eventCard").append(newCard);
         };
-
-
     }
-
-    // Initializes use of Materialize Modals
-    $(document).ready(function () {
-        $('.modal').modal();
-    });
-    // Opens Terms and Agreement on page load and reload
-    var windowTimeout = setTimeout(function () {
-        $("#modal1").modal('open');
-    }, 2000);
-
-    // Requires that user click checkbox to access "agree" button
-    $("#checkBoxInput").click(function () {
-        if ($(this).is(":checked")) {
-            $("#submitModalBtn").removeClass("disabled");
-        } else {
-
-            $("#submitModalBtn").addClass("disabled");
-        }
-    });
-
 });
 
 function addMarker(map, event) {
@@ -162,6 +138,23 @@ function addMarker(map, event) {
     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
 }
 
+// Initializes use of Materialize Modals
+$(document).ready(function () {
+    $('.modal').modal();
+});
+// Opens Terms and Agreement on page load and reload
+var windowTimeout = setTimeout(function () {
+    $("#modal1").modal('open');
+}, 2000);
 
+// Requires that user click checkbox to access "agree" button
+$("#checkBoxInput").click(function () {
+    if ($(this).is(":checked")) {
+        $("#submitModalBtn").removeClass("disabled");
+    } else {
+
+        $("#submitModalBtn").addClass("disabled");
+    }
+});
 
 
